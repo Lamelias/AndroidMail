12ad273dd96f0728abda7e1e8f2f011f50bd4fb8
lamelias <lamelias@163.com>
2020-03-03 14:44:52 +0800

    init

---
+++ libcommon/src/main/java/com/android/mail/utils/LogUtils.java
@@ -0,0 +1,63 @@
+package com.android.mail.utils;
+
+import android.os.RemoteException;
+
+public class LogUtils {
+    public static final String TAG = "LogUtils";
+
+    public static void e(String tag, String s) {
+
+    }
+
+    public static void e(String tag, String msg, Exception e) {
+
+    }
+
+    public static void e(String tag, String msg, Object... args) {
+
+    }
+
+    public static void e(String tag, Exception e, String msg) {
+
+    }
+
+    public static void d(String tag, String msg, Object... args) {
+
+    }
+
+    public static void d(String tag, Exception e, String msg) {
+
+    }
+
+    public static void i(String tag, String msg, Object... args) {
+
+    }
+
+    public static void w(String tag, String msg, Exception e) {
+
+    }
+
+    public static void w(String tag, Exception e, String msg) {
+
+    }
+
+    public static void w(String tag, String msg, Object... args) {
+
+    }
+
+    public static void wtf(String tag, String msg, Object... args) {
+
+    }
+
+    public static void wtf(String tag, Exception e, String msg) {
+
+    }
+
+    public static boolean isLoggable(String tag, int debug) {
+        return false;
+    }
+
+    public static void v(String tag, String msg, Object... args) {
+
+    }
+}
---
+++ libexchange/src/main/java/com/android/exchange/Configuration.java
@@ -0,0 +1,11 @@
+package com.android.exchange;
+
+public class Configuration {
+    public static final String EXCHANGE_ACCOUNT_MANAGER_TYPE = "com.android.exchange";
+    public static final String SYSTEM_PROVIDER_NAME = "com.android.email.provider";
+    public static final String APP_PROVIDER_NAME = "com.kingsoft.email.provider";
+    public static final String EXCHANGE_SERVICE_INTENT_ACTION =
+            "com.android.email.EXCHANGE_INTENT";
+    public static final String EXCHANGE_GAL_AUTHORITY = "com.android.exchange.directory.provider";
+    public static final String EXCHANGE_PROTOCOL = "eas";
+}
